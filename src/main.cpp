6256db32d9bcb95a814bf2ff417aa07ea6ca5b4e
vakimov <akimovvadi779@gmail.com>
2023-04-13 22:12:06 +0300

    Refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,6 @@
 #include "NetworkApi.h"
 
 int main(int argc, char *argv[]) {
-
-    printf("%s\n", zlibVersion());
-//    NetworkApi::getInstance().setPath("http://0.0.0.0:3000/packets_api/");
-//    NetworkApi::getInstance().getPacket("testlib.1.0");
-//    App* t = App::Run(argc, argv);
     App::getInstance()->run(argc, argv);
     return 0;
 }
